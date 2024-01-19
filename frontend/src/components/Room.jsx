acde4718326426f45c8122f555e0c733be93b411
Sarthak <sarthak19360@gmail.com>
2024-01-20 01:07:34 +0530

    Implemented webcam using webRTC APIs

--- frontend/src/components/Room.jsx
+++ frontend/src/components/Room.jsx
@@ -1,13 +1,60 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Room = () => {
+  const [toggleVideo, setToggleVideo] = useState(false);
+  const videoRef = useRef(null);
   const [searchParams, setSearchParams] = useSearchParams();
   const name = searchParams.get("name");
+
+  const playVideoFromCamera = async () => {
+    try {
+      const constraints = {
+        video: {
+          width: 640,
+          height: 480,
+        },
+      };
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      videoRef.current.srcObject = stream;
+    } catch (error) {
+      console.error("Error opening video camera.", error);
+    }
+  };
+
+  const stopStreamedVideo = async () => {
+    try {
+      const stream = await videoRef.current.srcObject;
+      const tracks = stream.getTracks();
+
+      tracks.forEach((track) => {
+        track.stop();
+      });
+
+      videoRef.current.srcObject = null;
+    } catch (error) {
+      console.error("Error closing video camera.", error);
+    }
+  };
+
   useEffect(() => {
-    // logic to initialize user to the room
+    playVideoFromCamera();
   }, []);
-  return <div>Hi {name}</div>;
+
+  return (
+    <div className="video-section">
+      <div>Hi {name}</div>
+      <video id="localVideo" ref={videoRef} autoPlay playsInline={false} />
+      <button
+        onClick={() => {
+          toggleVideo ? playVideoFromCamera() : stopStreamedVideo();
+          setToggleVideo(!toggleVideo);
+        }}
+      >
+        {toggleVideo ? "Open Camera" : "Close Camera"}
+      </button>
+    </div>
+  );
 };
 
 export default Room;
